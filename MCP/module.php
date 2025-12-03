1d6e63d0db1b67c6544a45fe805f188be1233674
Niels Thole <nt@symcon.de>
2025-12-03 17:20:29 +0100

    Add more tools

--- MCP/module.php
+++ MCP/module.php
@@ -147,6 +147,79 @@ class MCP extends IPSModuleStrict
                                 'additionalProperties' => false,
                                 '$schema' => 'http://json-schema.org/draft-07/schema#'
                             ]
+                        ],
+                        [
+                            'name' => 'get-snapshot',
+                            'title' => 'Get Symcon Snapshot',
+                            'description' => 'Get the snapshot of the Symcon system with data for all included objects.',
+                            'inputSchema' => [
+                                'type' => 'object',
+                                'additionalProperties' => false,
+                                '$schema' => 'http://json-schema.org/draft-07/schema#'
+                            ],
+                            'outputSchema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'snapshot' => [
+                                        'type' => 'object'
+                                    ]
+                                ],
+                                'required' => [
+                                    'snapshot'
+                                ],
+                                'additionalProperties' => false,
+                                '$schema' => 'http://json-schema.org/draft-07/schema#'
+                            ]
+                        ],
+                        [
+                            'name' => 'find-objects',
+                            'title' => 'Find Symcon Objects by using different kinds of filters',
+                            'description' => 'Find Symcon objects based on various criteria. The supported filters are type and name. Type is an array of possible object types (0: category, 1: instance, 2: variable, 3: script, 4: event, 5: media, 6: link). Name is a string that will be searched for in the object names (case insensitive, partial match). If multiple filters are provided, only objects matching all criteria will be returned. For each object, the full object info as returned by IPS_GetObject will be included in the output, containing ObjectID, ObjectName, ObjectType and more.',
+                            'inputSchema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'types' => [
+                                        'type' => 'array',
+                                        'items' => [
+                                            'type' => 'number'
+                                        ]
+                                    ],
+                                    'name' => [
+                                        'type' => 'string'
+                                    ]
+                                ],
+                                'required' => [],
+                                'additionalProperties' => false,
+                                '$schema' => 'http://json-schema.org/draft-07/schema#'
+                            ],
+                            'outputSchema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'objects' => [
+                                        'type' => 'array',
+                                        'items' => [
+                                            'type' => 'object',
+                                            'properties' => [
+                                                'ObjectID' => ['type' => 'number'],
+                                                'ObjectType' => ['type' => 'number'],
+                                                'ObjectName' => ['type' => 'string'],
+                                                'ObjectInfo' => ['type' => 'string'],
+                                                'ParentID' => ['type' => 'number'],
+                                                'ObjectPosition' => ['type' => 'number'],
+                                                'ObjectIsHidden' => ['type' => 'boolean'],
+                                                'ObjectIsReadOnly' => ['type' => 'boolean'],
+                                            ],
+                                            'required' => ['ObjectID', 'ObjectType', 'ObjectName', 'ObjectInfo', 'ParentID', 'ObjectPosition', 'ObjectIsHidden', 'ObjectIsReadOnly'],
+                                            'additionalProperties' => true,
+                                        ]
+                                    ]
+                                ],
+                                'required' => [
+                                    'objects'
+                                ],
+                                'additionalProperties' => false,
+                                '$schema' => 'http://json-schema.org/draft-07/schema#'
+                            ]
                         ]
                     ]
                 ];
@@ -164,6 +237,28 @@ class MCP extends IPSModuleStrict
                     case 'get-object':
                         $content = [
                             'info' => IPS_GetObject($request['params']['arguments']['objectID'])
+                        ];
+                        break;
+
+                    case 'get-snapshot':
+		                ini_set('ips.output_buffer', 20*1024*1024);
+                        $content = [
+                            'snapshot' => IPS_GetSnapshot()
+                        ];
+                        break;
+
+                    case 'find-objects':
+                        $searchName = strtolower($request['params']['arguments']['name'] ?? '');
+                        $types = $request['params']['arguments']['types'] ?? [0,1,2,3,4,5,6];
+                        $result = [];
+
+                        foreach(json_decode(IPS_GetSnapshot(), true)['objects'] as $index => $object) {
+                            if (str_contains(strtolower($object['name']), $searchName) && in_array($object['type'], $types)) {
+                                $result[] = IPS_GetObject(intval(substr($index, 2)));
+                            }
+                        }
+                        $content = [
+                            'objects' => $result
                         ];
                         break;
 
